db54c179f5a9e26c4ee47fa319ed92505f5e924c
faisal <faisaldevbhaluka@gmail.com>
2024-01-25 00:19:44 +0600

    now create ramdals dk webshop design in html css boostrap and another

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+"use strict";
+
+    $(window).on('scroll', function () {
+      var scroll = $(window).scrollTop();
+      if (scroll < 245) {
+        $(".header-sticky").removeClass("sticky");
+      } else {
+        $(".header-sticky").addClass("sticky");
+      }
+    });
+
+      $(document).ready(function(){
+          $('.full-image-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            doots: false,
+            prevArrow: '<span class="priv_arrow"><i class="fa-solid fa-angle-left"></i></span>',
+            nextArrow: ' <span class="next_arrow"><i class="fa-solid fa-angle-right"></i></span>',
+          });
+         
+      });  
+
+
+// WOW active
+new WOW().init();
+
+
+})(jQuery);
